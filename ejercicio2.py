47a2ea4d8bbf2b94872e966485de086b854a3383
Gustavo Fontana <117466366+GusFontana404@users.noreply.github.com>
2023-11-27 01:12:35 -0300

    Resolución ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 """## **Ejercicio 2**
 
 ### **Detección de patentes**
@@ -77,16 +78,16 @@ def mostrar_resultados(imagenes,f,c):
             img = imagenes[i * c + j]
             axs[i, j].imshow(img)
             axs[i, j].axis('off')
-
+    #Plotear imágenes
     plt.show()
 
 """### **Item b**"""
 
-def segmentar_patentes(patente):
+def segmentar_patentes(img):
   """Recibe la imagen de una patente y retorno la segmentación
   de los valores dentro de ella aplicando transformaciones."""
   #Pasar a gris la imagen
-  gray_roi = cv2.cvtColor(patente, cv2.COLOR_BGR2GRAY)
+  gray_roi = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
   #Binarizar la imagen
   imagen_binaria = np.where(gray_roi > 121, 1, 0)
@@ -96,11 +97,20 @@ def segmentar_patentes(patente):
   num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(imagen_binaria,8, cv2.CV_32S)
   im_color = cv2.applyColorMap(np.uint8(255/num_labels*labels), cv2.COLORMAP_JET)
 
+  # Lista para almacenar las ROIs
+  rois = []
+  patente_copia = img.copy()
+
   #Dibujar rectángulos
   for st in stats:
-    if st[4] >= 16 and st[4] <= 100 and st[1] > 0 and st[0] > 0:
-      bounding = cv2.rectangle(patente,(st[0],st[1]),(st[0]+st[2],st[1]+st[3]),color=(255,0,0),thickness=1)
-  return imagen_binaria, bounding
+    if st[0] > 0 and st[4] >= 16 and st[4] <= 100 and st[1] > 0:
+      # Extraer la ROI y agregar a la lista
+      roi = imagen_binaria[st[1]:st[1]+st[3], st[0]:st[0]+st[2]]
+      rois.append(roi)
+      #Dibujar los rectángulos en la componente conectada
+      bounding = cv2.rectangle(patente_copia,(st[0],st[1]),(st[0]+st[2],st[1]+st[3]),color=(255,0,0),thickness=1)
+
+  return imagen_binaria, bounding, rois, img
 
 """# **Mostrar resultados:**"""
 
@@ -112,28 +122,62 @@ imagen_patente = [f'{ruta}img0{n}.png' if n < 10 else f'{ruta}img{n}.png' for n 
 
 #Segmento las imágenes y las guardo
 patentes = [obtener_patentes(img)[5] for img in imagen_patente];
-copia_patentes = [obtener_patentes(img)[5] for img in imagen_patente];
 
 #Segmento las patentes y las guardo
-lista_patentes = [patentes[1],patentes[3],patentes[4],patentes[5],patentes[6],patentes[8],patentes[9]]
+copia_patentes = patentes.copy();
+lista_patentes = [copia_patentes[1],copia_patentes[3],copia_patentes[4],copia_patentes[5],copia_patentes[6],copia_patentes[8],copia_patentes[9]]
 patentes_segmentadas = [segmentar_patentes(img)[1] for img in lista_patentes];
 
+#Todas las patente procesadas
+todas_las_patentes = [patentes[0],patentes_segmentadas[0],patentes[2],patentes_segmentadas[1],patentes_segmentadas[2],patentes_segmentadas[3],
+                      patentes_segmentadas[4],patentes[7],patentes_segmentadas[5],patentes_segmentadas[6],patentes[10],patentes[11]]
+
+"""### **Ejemplo de procesamiento de una imagen**"""
+
+img, sob, dil, aper, bound, roi = obtener_patentes(imagen_patente[0])
+
 #Mostrar la imagen original y los resultados
-plt.figure(figsize=(15,8),facecolor='#333333')
-plt.subplot(231), plt.imshow(obtener_patentes(imagen_patente[0])[0], cmap='gray'), plt.title('Imagen original',color='white'),plt.axis('off')
-plt.subplot(232), plt.imshow(obtener_patentes(imagen_patente[0])[1], cmap='gray'), plt.title('Imagen con Sobel',color='white'),plt.axis('off')
-plt.subplot(233), plt.imshow(obtener_patentes(imagen_patente[0])[2], cmap='gray'), plt.title('Imagen dilatada',color='white'),plt.axis('off')
-plt.subplot(234), plt.imshow(obtener_patentes(imagen_patente[0])[3], cmap='gray'), plt.title('Imagen con morfología (apertura)',color='white'),plt.axis('off')
-plt.subplot(235), plt.imshow(obtener_patentes(imagen_patente[0])[4], cmap='gray'), plt.title('Componentes conectadas',color='white'),plt.axis('off')
-plt.subplot(236), plt.imshow(obtener_patentes(imagen_patente[0])[5], cmap='gray'), plt.title('ROI',color='white'),plt.axis('off')
+plt.figure(figsize=(16,8),facecolor='#333333')
+plt.subplot(231), plt.imshow(img, cmap='gray'), plt.title('Imagen original',color='white'),plt.axis('off')
+plt.subplot(232), plt.imshow(sob, cmap='gray'), plt.title('Imagen con Sobel',color='white'),plt.axis('off')
+plt.subplot(233), plt.imshow(dil, cmap='gray'), plt.title('Imagen dilatada',color='white'),plt.axis('off')
+plt.subplot(234), plt.imshow(aper, cmap='gray'), plt.title('Imagen con morfología (apertura)',color='white'),plt.axis('off')
+plt.subplot(235), plt.imshow(bound, cmap='gray'), plt.title('Componentes conectadas',color='white'),plt.axis('off')
+plt.subplot(236), plt.imshow(roi, cmap='gray'), plt.title('ROI',color='white'),plt.axis('off')
 plt.subplots_adjust(hspace=0.1)
 plt.show()
 
+"""### **Ejemplo de una patente procesada**"""
+
+patente_plot = patentes.copy()
+img_bin, bounding, rois, patente = segmentar_patentes(patente_plot[4])
+
+#Mostrar la imagen original y los resultados
+plt.figure(figsize=(15,5),facecolor='#333333')
+plt.subplot(131), plt.imshow(patente, cmap='gray'), plt.title('Imagen original',color='white'),plt.axis('off')
+plt.subplot(132), plt.imshow(img_bin, cmap='gray'), plt.title('Imagen binaria',color='white'),plt.axis('off')
+plt.subplot(133), plt.imshow(bounding, cmap='gray'), plt.title('Componentes conectadas',color='white'),plt.axis('off')
+plt.subplots_adjust(hspace=0.1)
+plt.show()
+
+"""### **Todas las imágenes segmentadas**"""
+
+mostrar_resultados(patentes,4,3)
+
+"""### **Bounding box sobre las patentes**"""
+
+mostrar_resultados(todas_las_patentes,4,3)
+
+"""### **Ejemplo de segmentar una patente**"""
+
 #Mostrar la imagen original y los resultados
 plt.figure(figsize=(15,8),facecolor='#333333')
-plt.subplot(231), plt.imshow(copia_patentes[4], cmap='gray'), plt.title('Imagen original',color='white'),plt.axis('off')
-plt.subplot(232), plt.imshow(segmentar_patentes(patentes[4])[0], cmap='gray'), plt.title('Imagen binaria',color='white'),plt.axis('off')
-plt.subplot(233), plt.imshow(patentes[4], cmap='gray'), plt.title('Componentes conectadas',color='white'),plt.axis('off')
+plt.subplot(161), plt.imshow(rois[5], cmap='gray'), plt.title('Caracter 1',color='white'),plt.axis('off')
+plt.subplot(162), plt.imshow(rois[4], cmap='gray'), plt.title('Caracter 2',color='white'),plt.axis('off')
+plt.subplot(163), plt.imshow(rois[3], cmap='gray'), plt.title('Caracter 3',color='white'),plt.axis('off')
+plt.subplot(164), plt.imshow(rois[2], cmap='gray'), plt.title('Caracter 4',color='white'),plt.axis('off')
+plt.subplot(165), plt.imshow(rois[1], cmap='gray'), plt.title('Caracter 5',color='white'),plt.axis('off')
+plt.subplot(166), plt.imshow(rois[0], cmap='gray'), plt.title('Caracter 6',color='white'),plt.axis('off')
 plt.subplots_adjust(hspace=0.1)
 plt.show()
 
